dc90331addfa484a0e5b9b132c30bc0d819328e5
Grigoriev.Vladimir <Hronos.Black@gmail.com>
2024-11-08 10:27:07 +0500

    Отслеживание выбранных жанров

--- assets/panels/searchpanel.py
+++ assets/panels/searchpanel.py
@@ -1,6 +1,6 @@
 import wx
+import wx.lib.agw.customtreectrl as wxc
 
-from wx.lib.agw.customtreectrl import CustomTreeCtrl, GenericTreeItem
 from ATLibrary.core import AT
 from ATLibrary.objects import Genre
 
@@ -11,9 +11,11 @@ class SearchPanel(wx.Panel):
         self.at: AT = kwargs.pop('at')
         wx.Panel.__init__(self, **kwargs)
         
+        self.checkGenres: list[Genre] = []
+        
         mainSizer = wx.BoxSizer(wx.HORIZONTAL)
         
-        self.treeGenres = CustomTreeCtrl(self)
+        self.treeGenres = wxc.CustomTreeCtrl(self)
         self.LoadGenres()
         mainSizer.Add(self.treeGenres, 1, wx.ALL | wx.EXPAND, 5)
         
@@ -24,18 +26,18 @@ class SearchPanel(wx.Panel):
         detailSizer.Add((0, 0), 0, 0, 0)
         detailSizer.Add((0, 0), 0, 0, 0)
         
-        mainSizer.Add(detailSizer, 1, wx.ALL | wx.EXPAND, 5)
+        mainSizer.Add(detailSizer, 3, wx.ALL | wx.EXPAND, 5)
         
         self.SetSizer(mainSizer)
         
         self.Layout()
         
     def LoadGenres(self) -> None:
-        rootItem: GenericTreeItem = self.treeGenres.AddRoot("Жанры:")
+        rootItem: wxc.GenericTreeItem = self.treeGenres.AddRoot("Жанры:")
         allGenres = self.at.AllGenres
         self.__add_genres_to_tree__(allGenres, rootItem)
         
-    def __add_genres_to_tree__(self, genres: list[Genre], parent: GenericTreeItem) -> None:
+    def __add_genres_to_tree__(self, genres: list[Genre], parent: wxc.GenericTreeItem) -> None:
         for genre in genres:
             item = self.treeGenres.AppendItem(
                 parentId=parent,
@@ -43,5 +45,14 @@ class SearchPanel(wx.Panel):
                 ct_type=1,
                 data=genre
             )
+            self.Bind(wxc.EVT_TREE_ITEM_CHECKED, self.__check__genre__)
             if len(genre.Childs) > 0:
                 self.__add_genres_to_tree__(genre.Childs, item)
+                
+    def __check__genre__(self, event) -> None:
+        item: wxc.GenericTreeItem = event.GetItem()
+        genre: Genre = item.GetData()
+        if item.IsChecked():
+            self.checkGenres.append(genre)
+        else:
+            self.checkGenres.pop(self.checkGenres.index(genre))
